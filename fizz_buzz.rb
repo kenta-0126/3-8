0a807ff4e2b90631141eda01a2ea0bb79efbfa26
EC2 Default User <ec2-user@ip-172-31-68-116.ec2.internal>
2021-05-08 11:05:57 +0000

    [Fix]FizzBuzzの出力コードの優先順位の変更[Finish]FizzBuzzプログラムを作ろう

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,10 +1,10 @@
 def fizz_buzz(number)
-  if number % 3 == 0
-    "Fizz"
+  if number % 15 == 0
+    "FizzBuzz"
     elsif number % 5 == 0
       "Buzz"
-    elsif number % 15 == 0
-      "FizzBuzz"
+    elsif number % 3 == 0
+      "Fizz"
     else
       number.to_s
      end
